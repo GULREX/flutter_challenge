654035841818ea3c671d9e929ab7cd49bee3e913
Gulrex <gulrexj@gmail.com>
2026-01-05 06:43:12 -0800

    2-scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
-      ),
+      home: Scaffold(
+        appBar: AppBar(
+        
+          title: Text("Workboard"),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+        body: Center(
+          child: Text("this is body area of scaffold widget"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Icon(Icons.add),
+          ),
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                ListTile(
+                  title: Text("about us"),
+                ),
+                ListTile(
+                  title: Text("contact us"),
+                ),
+              ],
+            ),
+          ),
+          
+          ),
     );
   }
 }
